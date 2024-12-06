f74e1794a3d5c4b54aef5792d57914959a6dbfa5
Prof Ahsan <ahoque707@gmail.com>
2024-12-06 20:22:40 +0600

    DBCON and Login Design
    Login Design

--- dobcon.php
+++ dobcon.php
@@ -6,31 +6,31 @@ $dbname ="summer24"; // Different real life
 
 // OO WAY 
 
-$conn = new mysqli($servername,$username,$password,$dbname); 
+/*$conn = new mysqli($servername,$username,$password,$dbname); 
 
 if ($conn->connect_error)
   die("<br>Connection FAILED WITH ERROR <br>".$conn->connect_error); 
 else 
   echo "<br><h1>Yahooooooo... Database Connected</h1>"; 
-
+*/
   
 // PRO WAY 
-/*
+
 $pcon = mysqli_connect($servername,$username,$password,$dbname); 
 
 if (!$pcon)
   die("<br> <h4>FAILED CONNECTION</h4> ".mysqli_connect_error());
 else 
-  echo "<br> <h4>Procedural Successfull Connection </h4>"; 
-*/
+  echo "<br> <h1>Procedural WAY Successfully Connected </h1>"; 
 
-// PDO 
+
+// PDO PHP Data Object  
 /*
 try
 {
  $pdocon = new PDO("mysql:host=$servername; dbname=$dbname",$username,$password);
  $pdocon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- echo "<br> PDO CONNECTED";  
+ echo "<br><h1> PDO WAY CONNECTED </h1>";  
 }
 catch(PDOException $e) 
 {
--- login.php
+++ login.php
@@ -10,11 +10,28 @@
    <h1>LOGIN</h1>
 
    <form method="POST" action="logchk.php">
-     USER NAME <input type="text" name="username" id="uid" value="" placeholder="username" required><br>
-     PASSWORD <input type="password" name="password" value="" placeholder="password" required><br>
-     ...........   <input type="submit" value="Sign IN">
+     <br>USER NAME <input type="text" name="username" id="uid" value="" placeholder="username" required>
+     <br>PASSWORD <input type="password" name="password" value="" placeholder="password" required>
+     <br><input type="submit" value="Sign IN">
 
   </form>
 
+  
+  <form method="POST" action="logchk.php">
+     <table>
+     <tr>
+      <td>User name</td> 
+       <td><input type="text" name="username" id="uid" value="" placeholder="username" required></td>
+      </tr>
+     <tr>
+       <td>Password</td>
+       <td><input type="password" name="password" value="" placeholder="password" required>
+      </td>
+    </tr>
+    <tr>
+      <td></td><td><input type="submit" value="Sign IN"></td>
+    </tr>
+     </table>
+  </form>
 </body>
 </html>
