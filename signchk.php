7abec9ba94465939c48af423d50708d99baf0c3b
Prof Ahsan <ahoque707@gmail.com>
2025-01-19 01:33:38 +0600

    Update Prepare
    prepare & bind param

--- signchk.php
+++ signchk.php
@@ -4,6 +4,7 @@ include 'dobcon.php';
  $uname = $_POST['username'];
  $upass = $_POST['password'];
 
+
 //1st way
 //$sql1 = "SELECT * FROM user WHERE usrname = '$uname' AND usrpass = '$upass'";
 $sql1 = "SELECT * FROM user WHERE usrname ='".$uname."' AND usrpass ='".$upass."'";
@@ -48,11 +49,21 @@ foreach ($result as $row)
 
 
 
-/* // Prepared Statement
-$psql = $mysqli->prepare("SELECT * FROM user WHERE usrname = ? AND usrpass = ?");
+// 3rd way 
+// Prepared Statement
+/*
+$psql = $conn->prepare("SELECT * FROM user WHERE usrname = ? AND usrpass = ?");
 $psql->bind_param("ss", $uname, $upass); //string, string
 $psql->execute();
 $result = $psql->get_result();
+if ($result->num_rows > 0) 
+{
+    echo "<br> <h1> Login Successful 3</h1>";
+}
+else 
+{
+    echo "<br> <h1> Login Failed, Invalid User Name or Password! </h1>";
+}
 */
 
 $conn->close();
